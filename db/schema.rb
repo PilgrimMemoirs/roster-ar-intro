a52626a2cfa7768878f0b5f05ebe5ff15ae00265
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-09-28 12:18:30 -0700

    student controller, index shows all students in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928172216) do
+ActiveRecord::Schema.define(version: 20160928174342) do
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20160928172216) do
     t.datetime "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "hobby"
   end
 
 end
